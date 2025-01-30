072226cd709988be82619d9e13adfad5625d990b
mubashshirkhan <mubashshirk7666@gamil.com>
2025-01-30 18:43:34 +0530

    delted all files

--- mk-loops.py
+++
@@ -1,9 +0,0 @@
-# Example of a for loop
-for i in range(5):
-    print(f"For loop iteration {i}")
-
-# Example of a while loop
-count = 0
-while count < 5:
-    print(f"While loop iteration {count}")
-    count += 1
