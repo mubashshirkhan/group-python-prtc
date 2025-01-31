b247bcd10d8edf1a6ee933435d2d8e8ac37d4795
sanawarraheman <sanawarraheman893@gmail.com>
2025-01-31 10:53:35 +0530

    thride commit

---
+++ sr-while-loop.py
@@ -0,0 +1,9 @@
+# Initial inventory
+inventory = {"apple": 10, "banana": 5, "orange": 8}
+
+# While loop: Decrease inventory until it's empty
+while inventory["apple"] > 0:
+    print(f"Selling an apple. Remaining: {inventory['apple']}")
+    inventory["apple"] -= 1
+
+print("Apples are out of stock!\n")
