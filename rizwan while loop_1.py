721b4bbcb16419118b36343ffb49ae28af39c9a4
umed khan <umedkhan@umeds-MacBook-Air.local>
2025-01-31 11:15:30 +0530

    second commit

--- rizwan while loop_1.py
+++ rizwan while loop_1.py
@@ -1,6 +1,11 @@
-#1 while loop decrease inventory until it's empty 
-invevtory = {"apple":10,"banana":5,"orange":8}
+inventory={"apple": 10, "banana": 5, "orange": 8}
+
+#1. While loop: Decrease inventory until it's empty
+
 while inventory["apple"] > 0:
-    print(f"selling an apple. remaining: {inventory["apple"]}")
-    inventory["apple"] -= 1
-    print("apples are out of stock!\n")
+
+    print(f"Selling an apple. Remaining: (inventory['apple'])")
+
+    inventory["apple"] -- 1
+
+print("Apples are out of stock!\n")
