faa3b5cb6881f2832155b0d5a632b49767d5de3c
mubashshirkhan <mubashshirk7666@gamil.com>
2025-02-01 11:51:31 +0530

    resolve and continues loop error& C:/Users/mubas_2410dna/.virtualenvs/AI-MEDICAL-CHATBOT-v_ntttEa/Scripts/python.exe c:/Users/mubas_2410dna/OneDrive/Desktop/coding/mk-forlloopin-if.py

--- mk-if-forloop.py
+++ mk-if-forloop.py
@@ -2,3 +2,4 @@ inventory = {"apple": 10, "banana": 5, "orange": 8}
 for fruit, stock in inventory.items():
     if stock < 6:  # Proper indentation
         print(f"Warning: Low stock of {fruit} ({stock} remaining)")  # Fixed f-string syntax
+        break
